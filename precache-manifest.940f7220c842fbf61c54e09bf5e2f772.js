b9fa4091364df012b12777dca68a51c0ca0e4734
Sangchul <scpark71@gmail.com>
2020-03-06 14:56:47 +0900

    Updates

---
+++ precache-manifest.940f7220c842fbf61c54e09bf5e2f772.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01247aacf4caaa0aff7993ca74a41113",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "09bbcfcf394900cfe701",
+    "url": "/todo-app/static/css/main.5663f211.chunk.css"
+  },
+  {
+    "revision": "5103785956ac25356378",
+    "url": "/todo-app/static/js/2.f5febf85.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/todo-app/static/js/2.f5febf85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bbcfcf394900cfe701",
+    "url": "/todo-app/static/js/main.17565459.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
